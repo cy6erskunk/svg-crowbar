502c24f72f9483161311db4b71ece64050cea214
Jiahao <jiahao.chen@mail.mcgill.ca>
2020-11-26 15:47:02 +0800

    remove ( ; ) to satisfy prettier

--- src/png.js
+++ src/png.js
@@ -2,11 +2,11 @@ import {commenceDownload} from './util'
 import {DEFAULT_FILENAME} from './const'
 
 function toBinary(string) {
-  const codeUnits = new Uint16Array(string.length);
+  const codeUnits = new Uint16Array(string.length)
   for (let i = 0; i < codeUnits.length; i++) {
-    codeUnits[i] = string.charCodeAt(i);
+    codeUnits[i] = string.charCodeAt(i)
   }
-  return String.fromCharCode(...new Uint8Array(codeUnits.buffer));
+  return String.fromCharCode(...new Uint8Array(codeUnits.buffer))
 }
 
 function downloadPng(source, filename = DEFAULT_FILENAME) {
