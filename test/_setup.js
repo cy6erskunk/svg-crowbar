a3f27efd1631a358f58a124a18c01ac9d4344827
Igor <cyberskunk@gmail.com>
2023-04-03 12:00:03 +0000

    Fix eslint

--- test/_setup.js
+++ test/_setup.js
@@ -2,8 +2,8 @@ import {prefix} from '../src/const'
 
 global.createSVG = () => document.createElementNS(prefix.svg, 'svg')
 Object.defineProperty(global, 'URL', {
-    writable: true,
-    value: {
-        createObjectURL: jest.fn()
-    },
-});
+  writable: true,
+  value: {
+    createObjectURL: jest.fn(),
+  },
+})
